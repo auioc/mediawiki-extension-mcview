b94a0e78d876d0067d5502f20ef1799a281b0f25
WakelessSloth56 <wsloth56@foxmail.com>
2021-06-05 21:12:50 +0800

    eslint, jsconfig

---
+++ resources/.eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jquery: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {},
+};
